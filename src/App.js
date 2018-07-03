c508ecacdfea79ca1e96d0345cd723b04c113079
Robby Dore <rdore88@gmail.com>
2018-07-03 13:46:47 -0400

    Refector and now first screencast

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home'
 import Login from './components/Login'
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducer from './reducers/starwarsReducer';
-
 class App extends Component {
   render() {
     return (
